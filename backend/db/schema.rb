a23864071229d0aa721b506ce09151927b37fe1d
Phyllis Kan <Phyllis Kan>
2020-06-13 18:22:26 -0500

    setting up session for backend

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_06_12_000049) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(username: "phyllis", password: "123456")
+User.create(username: "phyllis", email: "phyllis@gmail.com", password: "123456")
