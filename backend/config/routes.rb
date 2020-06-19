99fe92fedafd7f9a10d9ca1b1a2b0a867535ec80
Phyllis Kan <Phyllis Kan>
2020-06-19 15:52:12 -0500

    update routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
 
-  root 'pages#index'
+  # root 'pages#index'
+
+  
 
   namespace :api do
     namespace :v1 do
-      resources :pets
-      resources :diaries, only: [:create, :update, :destroy]
+      resources :pets do 
+        resources :diaries
+      end
     end
   end
 
+  gets '/diaries', to: 'diaries#all_diaries'
+
   # route request that are not for existing path predefined in our API back to our index path
-  get '*path', to: 'pages#index', via: :all
+  # get '*path', to: 'pages#index', via: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
