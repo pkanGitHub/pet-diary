e1289716f5d691ac94aff50b08b7222389a73f07
Phyllis Kan <Phyllis Kan>
2020-06-20 15:29:19 -0500

    create PetForm styling and component

---
+++ frontened/src/components/pets/PetForm.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const PetForm = (props) => {
+
+
+    return (
+        <div class="card">
+            <div class="container">
+                <h2 class="header">Pet Information</h2>
+                <form class="pet-form">
+                    <div class="name-field">
+                        <label>Name: </label>
+                        <input type="text" />
+                    </div>
+                    <br />
+                    <div class="birthdate-field">
+                        <label>Birth Date: </label>
+                        <input placeholder="mm/dd/yyyy" type="text" />
+                    </div>
+                    <br />
+                    <div class="extra content">
+                        <button class="btn info">Add</button>
+                        <button class="btn danger">Cancel</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default PetForm;
+
+
+
