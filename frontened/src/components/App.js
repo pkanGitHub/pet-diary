1952454afabf661a8330620fc502ada0d27ec84d
Phyllis Kan <Phyllis Kan>
2020-06-19 21:36:20 -0500

    change to functional component

--- frontened/src/components/App.js
+++ frontened/src/components/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { selectDiary } from '../actions/index'
+// import { selectDiary } from '../redux/actions/index'
 import { Route, Switch } from 'react-router-dom'
+import Home from './Home'
 
-class App extends React.Component() {
-    render() {
-        return (
-            <div>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/" component={AddPet} />
-                    <Route exact path="/" component={Diary} />
-                </Switch>
-            </div>
-        )
-    }
+const App = () => {
+    return (
+        // <NavLink />
+        <div>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                {/* <Route exact path="/" component={AddPet} />
+                    <Route exact path="/" component={Diary} /> */}
+            </Switch>
+        </div>
+    )
 }
 
 export default App;
