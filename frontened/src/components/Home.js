7575818f9ab8c767f85671229890cf65f0aa2543
Phyllis Kan <Phyllis Kan>
2020-07-02 13:34:02 -0500

    Update Home Page

--- frontened/src/components/Home.js
+++ frontened/src/components/Home.js
@@ -2,15 +2,19 @@ import React from 'react'
 import '../styles/NavBarStyle.css'
 import '../styles/App.css'
 import '../styles/Home.css'
+// import homeBG from '../styles/homePics.jpg'
 
 
 const Home = () => {
     return (
-        <div className="home-container">
-            <div className="home-description">
-                <p>This is an App for those pet owners like me, who have experience so many lost, and sometimes you just want to go back from time to time and look for the memories you had with them</p>
+        <>
+            <div className="home-container">
+                {/* <img className="bg-img" src={homeBG} alt="HomeBG" /> */}
+                <div className="home-description">
+                    This is an App for those pet owners like me, who have experience so many lost, and sometimes you just want to go back from time to time and look for the memories you had with them
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
